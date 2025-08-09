62a79f607ccd0bc1c40950c787bff38c3a614549
Ma-wardany <wardany.dev@gmail.com>
2025-08-09 18:23:54 +0300

    finishing rating

--- Dealio.Services/Interfaces/IRatingServices.cs
+++ Dealio.Services/Interfaces/IRatingServices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dealio.Domain.Entities;
+using Dealio.Services.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,16 @@ namespace Dealio.Services.Interfaces
 {
     public interface IRatingServices
     {
+        public Task<ServiceResult<Rating>> AddRatingAsync(Rating rate);
+
+        public Task<ServiceResultEnum> DeleteRatingAsync(string userId);
+
+
+        public Task<ServiceResult<Rating>> UpdateRatingAsync(Rating rating);
+
+        public Task<ServiceResult<Rating>> GetUserRatingAsync(string userId);
+
+        public Task<ServiceResult<IQueryable<Rating>>> GetAllRatingsAsync();
+
     }
 }
